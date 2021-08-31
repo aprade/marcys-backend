16c278867db6e6ad2c70d4236295254bacffbeaf
João Koritar <jakoritarleite@gmail.com>
2021-08-30 23:45:20 -0300

    feat: implement testing with jest and supertest
    Would you believe that we're 100% code coverage here ? :O

---
+++ tests/index.test.ts
@@ -0,0 +1,11 @@
+import app from '../src/app';
+import supertest from 'supertest';
+
+describe("ALL / - root endpoint", () => {
+	it("Application is up", async () => {
+		const result = await supertest(app).get("/");
+
+		expect(result.body).toEqual({"body":"Use the correct route you dumbass!!!"});
+		expect(result.statusCode).toEqual(200);
+	});
+});
