a04e7e21823f66740654ea2a3c634f13ab54dc9a
JoaoVitorAmorim <almeidaamorimjoaovitor@gmail.com>
2021-08-31 20:32:03 -0300

    step: created ip validation

--- src/controllers/machines.ts
+++ src/controllers/machines.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import validator from "../modules/validator"
 
 interface Machine {
 	ip: string;
@@ -6,12 +7,18 @@ interface Machine {
 }
 
 const addMachine = async (req: Request<string, Machine>, res: Response): Promise<Response> => {
-	console.log('body', req.body);
-
+	if (!await validator.ipValidator(req.body.ip)){
+		return res.status(406).json({
+			message: 'Invalid IP',
+			status_code: 406,
+			machine: req.body,
+		});
+	}
 	return res.status(200).json({
 		message: req.body
 	});
-}
+}	
+
 
 const getMachines = async (req: Request, res: Response): Promise<Response> => {
 	return res.status(200).json({
