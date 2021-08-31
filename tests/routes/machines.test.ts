2f3d84beddf46ff2f0c4508f63e04e8f99dad16c
JoaoVitorAmorim <almeidaamorimjoaovitor@gmail.com>
2021-08-31 20:32:03 -0300

    step: created ip validation

--- tests/routes/machines.test.ts
+++ tests/routes/machines.test.ts
@@ -3,15 +3,33 @@ import supertest from 'supertest';
 
 describe("POST /machines - create new machine endpoint", () => {
 	it("Successfuly create a new application", async () => {
+		const payload = {'ip': '0.0.0.0', 'nickname': 'localhost'};
 		const result = await supertest(app)
 			.post("/machines")
 			.set('Content-type', 'application/json')
-			.send({ 'ip': '0.0.0.0', 'nickname': 'localhost'});
+			.send(payload);
 
-		expect(result.body).toEqual({ message: { ip: '0.0.0.0', nickname: 'localhost' } });
+		expect(result.body).toEqual({ message: payload });
 		expect(result.statusCode).toEqual(200);
 	});
+
+	it("Failed while validating IP", async () => {
+		const payload = {'ip': '222.666.111.44', 'nickname': 'localhost'};
+		const result = await supertest(app)
+			.post("/machines")
+			.set('Content-type', 'application/json')
+			.send(payload);
+
+		expect(result.body).toEqual({
+			message: 'Invalid IP',
+			status_code: 406,
+			machine: payload,
+		});
+		expect(result.statusCode).toEqual(406);
+	});
 });
+
+
 
 describe("GET /machines - get all registered machines endpoint", () => {
 	it("Successfuly received the machines", async () => {
