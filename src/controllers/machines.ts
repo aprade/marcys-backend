8bc4ef0bd3034863bfab189b5aba35685b0c9c6f
João Koritar <jakoritarleite@gmail.com>
2021-09-02 17:18:10 -0300

    feat: code that adds it into the database

--- src/controllers/machines.ts
+++ src/controllers/machines.ts
@@ -1,18 +1,23 @@
 import { Request, Response } from 'express';
 
 import * as api from "../modules/api";
+import * as database from "../modules/database";
 import validator from "../modules/validator";
-interface Machine {
-	ip: string;
-	nickname: string;
+
+interface MachineResponse {
+	message: string;
+	status_code: number;
+	machine: database.Machine;
+	status: string;
 }
 
-const addMachine = async (req: Request<string, Machine>, res: Response): Promise<Response> => {
+const addMachine = async (req: Request<string, database.Machine>, res: Response): Promise<Response<MachineResponse>> => {
 	if (!await validator.ipValidator(req.body.ip))
 		return res.status(406).json({
 			message: "The provided IP address is invalid.",
 			status_code: 406,
 			machine: req.body,
+			status: "FAILED",
 		});
 
 	if (!await api.checkMachine())
@@ -20,13 +25,34 @@ const addMachine = async (req: Request<string, Machine>, res: Response): Promise
 			message: "Could not connect to the specified IP. Verify if it can be reached by the outside world",
 			status_code: 404,
 			machine: req.body,
+			status: "FAILED",
 		});
 
-	return res.status(200).json({
-		message: "Successfuly added the machine.",
-		status_code: 200,
-		machine: req.body,
-	});
+	let databaseResult = await database.setMachine(req.body);
+
+	switch (databaseResult) {
+		case database.SetMachineMessage.ADDED:
+			return res.status(200).json({
+				message: databaseResult,
+				status_code: 200,
+				machine: req.body,
+				status: "SUCCESS",
+			});
+		case database.SetMachineMessage.FAILED:
+			return res.status(500).json({
+				message: databaseResult,
+				status_code: 500,
+				machine: req.body,
+				status: "FAILED",
+			});
+		default:
+			return res.status(500).json({
+				message: "Unkown error type",
+				status_code: 500,
+				machine: req.body,
+				status: "FAILED",
+			});
+	}
 }
 
 const getMachines = async (req: Request, res: Response): Promise<Response> => {
