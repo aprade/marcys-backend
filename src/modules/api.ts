9fb025abdc9e366c54c0644bef4116a2b28e0709
João Koritar <jakoritarleite@gmail.com>
2021-09-08 20:34:11 -0300

    feat: get information from the registered machine and save as history

--- src/modules/api.ts
+++ src/modules/api.ts
@@ -2,6 +2,28 @@ import * as http from './http';
 
 export type CheckMachine = boolean;
 
+interface MachineCPULoadAverage {
+  one: number;
+  five: number;
+  fifteen: number;
+}
+
+export interface MachineCPUInfo {
+  frequency: number;
+  logical_cores: number;
+  physical_cores: number;
+  load_average: MachineCPULoadAverage;
+}
+
+export interface MachineMemoryInfo {
+  total: number;
+  free: number;
+  used: number;
+  shared: number;
+  buffers: number;
+  cached: number;
+}
+
 export const checkMachine = async (): Promise<CheckMachine> => {
   try {
     await http.get<string>('/');
@@ -11,3 +33,9 @@ export const checkMachine = async (): Promise<CheckMachine> => {
 
   return true;
 };
+
+export const getMachineCPUInfo = async (): Promise<MachineCPUInfo> =>
+  await http.get<MachineCPUInfo>('/cpu');
+
+export const getMachineMemoryInfo = async (): Promise<MachineMemoryInfo> =>
+  await http.get<MachineMemoryInfo>('/memory');
